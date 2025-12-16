3ec1ccc18febe4dad87904649e6974c949ddefe6
Sami Pasquali <5637087+samp1044@users.noreply.github.com>
2025-12-16 22:38:31 +0100

    WIP merged former home with about and started rewording the intro text

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-about',
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
     templateUrl: './about.component.html',
     styleUrl: './about.component.scss'
 })
