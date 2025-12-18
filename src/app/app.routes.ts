83df3c73753fec6bb22321a593cb24d64cfb9f56
Sami Pasquali <5637087+samp1044@users.noreply.github.com>
2025-12-18 21:21:49 +0100

    refactor: restructured aboutme page and made it default. Also added catch all at the end that redirects to about me

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,14 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from "./pages/home/home.component";
 import {ProjectsComponent} from "./pages/projects/projects.component";
 import {ResumeComponent} from "./pages/resume/resume.component";
 import {ContactComponent} from "./pages/contact/contact.component";
 import {AboutComponent} from "./pages/about/about.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: 'projects', component: ProjectsComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '/about' }
 ];
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -4,7 +4,7 @@ import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-about',
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule],
     templateUrl: './about.component.html',
     styleUrl: './about.component.scss'
 })
