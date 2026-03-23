cb0dad68670b7028c4a8cb85472e32da2731ecb6
Sami <5637087+samp1044@users.noreply.github.com>
2026-03-23 22:08:37 +0100

    Feature/7 make it work on the phone (#28)
    * centered the nav horizontally on large screens
    
    * centered page content (except CV) on large screens
    
    * moved nav to bottom navigation on small screens
    
    * merged /home and /about me
    
    * updated some text within the CV page

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
     selector: 'app-navigation',
-    imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
     templateUrl: './navigation.component.html',
     styleUrl: './navigation.component.scss'
 })
